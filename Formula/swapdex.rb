9076b07ac69826d7ab504804fa00e0fa4fe0d55a
KBS <youdie006@naver.com>
2026-08-25 15:19:59 +0900

    fix: formula URLs still pointed at v0.75.0 - every brew install failed

--- Formula/swapdex.rb
+++ Formula/swapdex.rb
@@ -5,19 +5,19 @@ class Swapdex < Formula
   license "MIT"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/youdie006/swapdex/releases/download/v0.75.0/swapdex-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/youdie006/swapdex/releases/download/v0.96.0/swapdex-aarch64-apple-darwin.tar.gz"
       sha256 "43353c46ac598a1f59268d9c23644d9f8eb878cf04a62bae7a5348801626c5c2"
     else
-      url "https://github.com/youdie006/swapdex/releases/download/v0.75.0/swapdex-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/youdie006/swapdex/releases/download/v0.96.0/swapdex-x86_64-apple-darwin.tar.gz"
       sha256 "e67803833d4d216f604e9d7bdc867754c63c1f11fa71b2f1a977e86f0ffe834c"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/youdie006/swapdex/releases/download/v0.75.0/swapdex-aarch64-unknown-linux-musl.tar.gz"
+      url "https://github.com/youdie006/swapdex/releases/download/v0.96.0/swapdex-aarch64-unknown-linux-musl.tar.gz"
       sha256 "8a2fc253d281db191ff81bd973fcb9e58d9df8e58c93ebc0f386e4a1d58fd25a"
     else
-      url "https://github.com/youdie006/swapdex/releases/download/v0.75.0/swapdex-x86_64-unknown-linux-musl.tar.gz"
+      url "https://github.com/youdie006/swapdex/releases/download/v0.96.0/swapdex-x86_64-unknown-linux-musl.tar.gz"
       sha256 "e214b754f83a92bbd9ab293176ac57d0b33bf304bbb9db3b06dfbab28c68eece"
     end
   end
